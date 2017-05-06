d18bf5b806ae6e5232419c21c49d86b0ed912903
suryatejreddy <suryatej16102@iiitd.ac.in>
2017-05-07 01:06:54 +0530

    adding my backend stuff

---
+++ angel/street/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url 
+from django.contrib import admin
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+	url(r'^$', views.start , name = 'start'),
+	url(r'^signup/$', views.signup , name = 'signup' ),
+	url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^accounts/profile/', views.home , name = 'home' ),
+    url(r'^performances/',views.performances,name = 'performances'),
+
+]
